e85dccda3b9b475ea2b4161ec20df6001a0367b4
Luc Steffens <luc.steffens@stecum.be>
2016-12-07 08:43:38 +0100

    First request to Prober API
    Request id created based on Postman code.
    
    - Added request-module using npm to the project
    - Added node_modules in .gitignore, dependent libs don't need to be in git 
    - Added support for invalid certificates

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
+// lucs@sabayon ~/tmp/ProberRequestNodeJs $ npm install --save request
+var request = require("request");
+
+// http://stackoverflow.com/questions/10888610/ignore-invalid-self-signed-ssl-certificate-in-node-js-with-https-request
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+var options = {method: 'GET',
+	url: 'https://web-ims.thomasmore.be/datadistribution/API/2.0/drones',
+	qs: {format: 'json'},
+	headers:
+		{authorization: 'Basic aW1zOno1MTJtVDRKeVgwUExXZw=='}};
+
+request(options, function (error, response, body) {
+	if (error)
+		throw new Error(error);
+
+	console.log(body);
+});
+
+
 console.log("Hello World!");
