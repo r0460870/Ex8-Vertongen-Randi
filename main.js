55ccd10b3cf156a32715354a55497dcee7c94ab8
Luc Steffens <luc.steffens@stecum.be>
2016-12-07 09:22:08 +0100

    Using Settings from html-prject

--- main.js
+++ main.js
@@ -4,18 +4,27 @@ var request = require("request");
 // http://stackoverflow.com/questions/10888610/ignore-invalid-self-signed-ssl-certificate-in-node-js-with-https-request
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-var options = {method: 'GET',
-	url: 'https://web-ims.thomasmore.be/datadistribution/API/2.0/drones',
-	qs: {format: 'json'},
-	headers:
-		{authorization: 'Basic aW1zOno1MTJtVDRKeVgwUExXZw=='}};
 
-request(options, function (error, response, body) {
+
+var BASE_URL = "https://web-ims.thomasmore.be/datadistribution/API/2.0";
+var Settings = function (url) {
+	this.url = BASE_URL + url;
+	this.method = "GET";
+	this.dataType = 'json';
+	this.headers = {
+		authorization: "Basic aW1zOno1MTJtVDRKeVgwUExXZw=="
+	};
+};
+
+var dronesSettings = new Settings("/drones?format=json");
+
+request(dronesSettings, function (error, response, body) {
 	if (error)
 		throw new Error(error);
 
+
+
 	console.log(body);
 });
-
 
 console.log("Hello World!");
