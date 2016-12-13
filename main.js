1381204be626b5a8c245dd95deae4889317f5e1d
Randi Vertongen <r0460870@student.thomasmore.be>
2016-12-13 20:52:54 +0100

    opschonen code & toevoegen startfunctie voor contents

--- main.js
+++ main.js
@@ -29,43 +29,38 @@ var Content = function (id, mac_address, datetime) {
     this._id = id;
     this.mac_address = mac_address;
     this.datetime = datetime;
-    this.rssi = rssi;
-    this.ref = ref;
-    this.url = url
-
 };
 
 
 var dronesSettings = new Settings("/drones?format=json");
-var filesSettings = new Settings("/files?format=json");
+var filesSettings = new Settings("/files?drone_id.is="+ drone.id +"format=json");
 dal.clearDrone();
 
 request(dronesSettings, function (error, response, dronesString) {
-	var drones = JSON.parse(dronesString);
-	console.log(drones);
-	console.log("***************************************************************************");
-	drones.forEach(function (drone) {
-		var droneSettings = new Settings("/drones/" + drone.id + "?format=json");
-		request(droneSettings, function (error, response, droneString) {
-			var drone = JSON.parse(droneString);
-			dal.insertDrone(new Drone(drone.id, drone.name, drone.mac_address));
+		var drones = JSON.parse(dronesString);
+		console.log(drones);
+		console.log("***************************************************************************");
+		drones.forEach(function (drone) {
+				var droneSettings = new Settings("/drones/" + drone.id + "?format=json");
+				request(droneSettings, function (error, response, droneString) {
+				var drone = JSON.parse(droneString);
+				dal.insertDrone(new Drone(drone.id, drone.name, drone.mac_address));
+
+				request(filesSettings, function (error, response, filesString) {
+						var files = JSON.parse(filesString);
+						console.log(files);
+						console.log("test");
+						console.log("***************************************************************************");
+						drones.forEach(function (file) {
+								var filesettings = new Settings("/files/" + file.id + "?format=json");
+								request(filesettings, function (error, response, fileString) {
+										var file = JSON.parse(filesString);
+										dal.insertFile(new File(file.id));
+										
 		});
 	});
 });
-
-request(filesSettings, function (error, response, fileString) {
-	var files = JSON.parse(fileString);
-	console.log(files);
-		console.log("test");
-	console.log("***************************************************************************");
-	drones.forEach(function (file) {
-		var filesettings = new Settings("/files/" + file.id + "?format=json");
-		request(filesettings, function (error, response, fileString) {
-			var file = JSON.parse(fileString);
-			dal.insertFile(new File(file.id));
-		});
-	});
 });
-
-
+});
+});
 console.log("Hello World!");
