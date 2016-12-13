5a1cbbb5d6269e93e688570036274ae994d74b38
Randi Vertongen <r0460870@student.thomasmore.be>
2016-12-13 21:56:03 +0100

    code van main.js lijkt te werken, maar mongodb op de storage niet echt

--- storage.js
+++ storage.js
@@ -1,11 +1,9 @@
 var MongoClient = require('mongodb').MongoClient;
-
+//npm install?
 var url = 'mongodb://localhost:27017/prober';
 // mongo in nodeJs: http://mongodb.github.io/node-mongodb-native/2.2/
 // mongo API : http://mongodb.github.io/node-mongodb-native/2.2/api/Collection.html
-
 var dal = {
-
 	connect: function (err, result) {
 		MongoClient.connect(url, function (error, db) {
 			if (error)
